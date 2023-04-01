d7288a661749305c7d27b38fc2229c46610393f6
Amanda Rocha <111551615+seugirdorx@users.noreply.github.com>
2023-04-01 10:47:37 -0300

    Aula 04 - Componente Sidebar

--- src/App.js
+++ src/App.js
@@ -5,10 +5,9 @@ function App() {
   return (
     <div className="App">
       <div className="app__videos">
-        <Video />
-        <Video />
-        <Video />
-        <Video />
+        <Video likes={543} messages={230} shares={186} name="Amanda" description="Gatinho pulando" music="Música muito épica" url="https://firebasestorage.googleapis.com/v0/b/jornada-b5e9d.appspot.com/o/brecker2.mp4?alt=media&token=9aa979e9-0907-4093-abd5-a71efe4fc946" />
+        <Video likes={329} messages={120} shares={647} name="Rocha" description="Gatinho confuso" music="Música muito lendária" url="https://firebasestorage.googleapis.com/v0/b/jornada-dev.appspot.com/o/bird.mp4?alt=media&token=52abbeec-ff95-4acb-808e-5a4b4977d1da" />
+       
       </div>
     </div>
   );
